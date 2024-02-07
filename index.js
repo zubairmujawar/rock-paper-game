f411237a4d4f204b231a486d3e8996f1d5a8f85d
Zubair Mujawar <mujawarzubair167@gmail.com.com>
2024-02-07 09:24:30 +0530

    issue with playGame function

--- index.js
+++ index.js
@@ -1,33 +1,57 @@
-const choices = document.querySelectorAll(".choices")
+const choices = document.querySelectorAll(".choices");
 
 let userScore = 0;
 let compScore = 0;
+// Genrate computer choice
+const compChoice = () => {
+  const values = ["rock", "paper", "scissors"];
+  let index = Math.floor(Math.random() * 3);
+  let Compvalue = values[index];
+  console.log("Computer Choice", Compvalue);
+};
+// Genrate user choice
+choices.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    let userChoice = choice.getAttribute("id");
+    console.log("user Choice is", userChoice); // new
+    compChoice();
+    PlayGame();
+    return userChoice;
+  });
+});
+// check the wining conditions
+const PlayGame = (userChoice) => {
+  if (compChoice === userChoice) {
+    // game draw
+    drawGame();
+  } else {
+    let userWins = false;
+    if (userChoice === "rock") {
+      // paper, scissors
+      userWins = compChoice === "paper" ? false : true;
 
-const compChoice = ()=>{
-    const values = ["rock", "paper", "scissors"]
-    let index = Math.floor(Math.random() * 3)
-    let Compvalue = values[index];
-    console.log("CompChose", Compvalue);
-}
-choices.forEach(choice=>{
-    choice.addEventListener("click", ()=>{
-        let userChoice = choice.getAttribute("id")
-        console.log("User choice is", userChoice)
-        compChoice();
-        PlayGame()
-        return userChoice;
-       
-    })
-})
+    } else if (userChoice === "paper") {
+      // rock, paper
+      userWins = compChoice === "scissors" ? false : true;
 
-const PlayGame = (userChoice)=>{
-    if(compChoice === "rock" && userChoice === "scissors"){
-        console.log("Comp Wins")
-    }else if( compChoice === "scissors" && userChoice === "paper"){
-        console.log("Comp Wins")
-    }else if(compChoice === "papeer" && userChoice === "rock"){
-        console.log("user wins")
-    }else{
-        console.log("Not working, or Draw")
+    } else {
+      // paper , scossors
+      userWins = compChoice === "rock" ? false : true;
     }
-}
+    showWiner(userWins);
+  }
+};
+
+// if game was draw
+const drawGame = () => {
+  console.log("The game was draw");
+};
+
+const showWiner = (userWins) => {
+  console.log("userWins", userWins);
+  if (userWins) {
+    console.log("User Wins!");
+  } else {
+    console.log("User Lose!");
+  }
+};

8fe93ad1f1075a2b0ec9af65e4f3fbcdf77044c5
Zubair Mujawar <mujawarzubair167@gmail.com.com>
2024-02-07 20:53:09 +0530

    completed

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const choices = document.querySelectorAll(".choices");
-
+const message = document.querySelector("#msg");
+const UserScore = document.querySelector("#UserScore");
+const CompScore = document.querySelector("#CompScore");
+const compMsg = document.querySelector("#compMsg");
 let userScore = 0;
 let compScore = 0;
 // Genrate computer choice
@@ -7,36 +10,38 @@ const compChoice = () => {
   const values = ["rock", "paper", "scissors"];
   let index = Math.floor(Math.random() * 3);
   let Compvalue = values[index];
-  console.log("Computer Choice", Compvalue);
+  return Compvalue;
 };
 // Genrate user choice
 choices.forEach((choice) => {
   choice.addEventListener("click", () => {
+    let computerChoice = compChoice();
     let userChoice = choice.getAttribute("id");
-    console.log("user Choice is", userChoice); // new
-    compChoice();
-    PlayGame();
-    return userChoice;
+    compMsg.innerText = `Computer Chose ${computerChoice}`;
+    PlayGame(userChoice, computerChoice);
   });
 });
 // check the wining conditions
-const PlayGame = (userChoice) => {
-  if (compChoice === userChoice) {
+const PlayGame = (userChoice, computerChoice) => {
+  // console.log("Computer Choice", computerChoice);
+  // console.log("user Choice", userChoice);
+  if (computerChoice === userChoice) {
     // game draw
     drawGame();
   } else {
-    let userWins = false;
+    let userWins = true;
     if (userChoice === "rock") {
       // paper, scissors
-      userWins = compChoice === "paper" ? false : true;
-
+      userWins = computerChoice === "paper" ? false : true;
+      // console.log(userWins, "userWins")
     } else if (userChoice === "paper") {
       // rock, paper
-      userWins = compChoice === "scissors" ? false : true;
-
+      userWins = computerChoice === "scissors" ? false : true;
+      // console.log(userWins, "userWins");
     } else {
       // paper , scossors
-      userWins = compChoice === "rock" ? false : true;
+      userWins = computerChoice === "rock" ? false : true;
+      // console.log(userWins, "userWins");
     }
     showWiner(userWins);
   }
@@ -44,14 +49,21 @@ const PlayGame = (userChoice) => {
 
 // if game was draw
 const drawGame = () => {
-  console.log("The game was draw");
+  // console.log("The game was draw");
+  message.innerText = "The game was draw";
 };
 
 const showWiner = (userWins) => {
-  console.log("userWins", userWins);
+  // console.log("userWins", userWins);
   if (userWins) {
-    console.log("User Wins!");
+    // console.log("User Wins!");
+    message.innerText = "You Wins!";
+    userScore++;
+    UserScore.innerText = userScore;
   } else {
-    console.log("User Lose!");
+    // console.log("User Lose!");
+    message.innerText = "You lose!";
+    compScore++;
+    CompScore.innerText = compScore;
   }
 };
